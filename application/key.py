ae64291d30d8cae8e0bc90096267cce2e51ee77e
Satan1437 <exemplles.white@gmail.com>
2022-01-05 15:01:37 +0300

    unlock key for django

---
+++ application/key.py
@@ -0,0 +1,11 @@
+SECRET_KEY = 'django-insecure-y81c=qsypok_l=#&8yqe2)md!h80-hkpzh($x#fh+2i$!@v-k!'
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'd5d5sku52208ng',
+		'USER': 'otqcpsbkravryl',
+		'PASSWORD': 'dc3619cb400730ef9171fbcf1e0270d70b6e015a59aaf5409dd4deaf24dbf3d7',
+		'HOST': 'ec2-34-255-225-151.eu-west-1.compute.amazonaws.com',
+	}
+}
